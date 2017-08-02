d8d5e7374845f44eb3f575c1ddcb5218d4fd3d3f
jbk77 <josephbk.117@hotmail.com>
2017-08-02 23:30:51 +0530

    Added seperate tab window for each shader type

--- GLSL_Editor/MainWindow.xaml.cs
+++ GLSL_Editor/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Windows.Shapes;
 namespace GLSL_Editor
 {
 
+    /* C++ file that takes string parameter for vertex and fragment shader, get console output from that */
+
     public partial class MainWindow : Window
     {
         Regex typesRegex = new Regex(@"(vec1|vec2|vec3|vec4|mat2|mat3|mat4|float|int|uint|double|bool|void|sampler1D|sampler2D|sampler3D|struct)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
@@ -24,32 +26,35 @@ namespace GLSL_Editor
         Regex commentRegex = new Regex(@"(\/\/.*)|(\/\*[\s\S]*\/)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         //Special Variables Only vertex shader
-        Regex vertexShaderSpecial = new Regex(@"\sgl_Position\s|\sgl_PointSize\s|\sgl_VertexID\s|\sgl_InstanceID\s", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        Regex vertexShaderSpecial = new Regex(@"gl_Position\s|gl_PointSize\s|gl_VertexID\s|gl_InstanceID\s", RegexOptions.Compiled);
         //Special Variables Only fragment shader
-        Regex fragmentShaderSpecial = new Regex(@"\sgl_FragCoord\s|\sgl_FrontFacing\s|\sgl_FragDepth\s", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        Regex fragmentShaderSpecial = new Regex(@"gl_FragCoord\s|gl_FrontFacing\s|gl_FragDepth\s", RegexOptions.Compiled);
 
         public MainWindow()
         {
-            InitializeComponent();           
+            InitializeComponent();
         }
         private void TextBox_keyUp(object sender, KeyEventArgs e)
         {
-            TextRange range = new TextRange(glslTextBox.Document.ContentStart, glslTextBox.Document.ContentEnd);
+            RichTextBox currentTextBox = (RichTextBox)sender;
+            Regex shaderSpecificRegex;
+            if (sender == glslVertexTextbox)
+                shaderSpecificRegex = vertexShaderSpecial;
+            else
+                shaderSpecificRegex = fragmentShaderSpecial;
+
+            TextRange range = new TextRange(currentTextBox.Document.ContentStart, currentTextBox.Document.ContentEnd);
             range.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(Colors.White));
 
-            
-            var start = glslTextBox.Document.ContentStart;
-            while (start != null && start.CompareTo(glslTextBox.Document.ContentEnd) < 0)
+            var start = currentTextBox.Document.ContentStart;
+            while (start != null && start.CompareTo(currentTextBox.Document.ContentEnd) < 0)
             {
                 if (start.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text)
                 {
                     var match = typesRegex.Match(start.GetTextInRun(LogicalDirection.Forward));
                     var match2 = miscRegex.Match(start.GetTextInRun(LogicalDirection.Forward));
                     var match3 = commentRegex.Match(start.GetTextInRun(LogicalDirection.Forward));
-
-                    //For now checking for both vertex and fragment soecific stuff
-                    var match4 = vertexShaderSpecial.Match(start.GetTextInRun(LogicalDirection.Forward));
-                    var match5 = fragmentShaderSpecial.Match(start.GetTextInRun(LogicalDirection.Forward));
+                    var match4 = shaderSpecificRegex.Match(start.GetTextInRun(LogicalDirection.Forward));
 
                     if (match.Length > 0)
                     {
@@ -72,12 +77,6 @@ namespace GLSL_Editor
                     else if (match4.Length > 0)
                     {
                         var textrange = new TextRange(start.GetPositionAtOffset(match4.Index, LogicalDirection.Forward), start.GetPositionAtOffset(match4.Index + match4.Length, LogicalDirection.Backward));
-                        SolidColorBrush colourBrush = new SolidColorBrush(Color.FromRgb(100, 255, 255));
-                        textrange.ApplyPropertyValue(TextElement.ForegroundProperty, colourBrush);
-                    }
-                    else if (match5.Length > 0)
-                    {
-                        var textrange = new TextRange(start.GetPositionAtOffset(match5.Index, LogicalDirection.Forward), start.GetPositionAtOffset(match5.Index + match5.Length, LogicalDirection.Backward));
                         SolidColorBrush colourBrush = new SolidColorBrush(Color.FromRgb(100, 255, 255));
                         textrange.ApplyPropertyValue(TextElement.ForegroundProperty, colourBrush);
                     }
